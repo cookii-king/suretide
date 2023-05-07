19d7029c01c78950e4755c4e6fa1824ecd359f25
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-07 23:21:03 +0100

    Update setup-part-3.sh

--- setup-part-3.sh
+++ setup-part-3.sh
@@ -1,42 +1,15 @@
 #!/bin/bash
 
-# Set terminal type to xterm
-export TERM=xterm
-SFTP_LINE="/usr/bin/sftp"
-ENVIRONMENT_SHELL="/bin/bash"
-
-# Determine the line length of the terminal
-LINE_LENGTH=$(/usr/bin/tput cols)
-CURRENT_USER="$(whoami)"
-# Set the base path for the script
 BASE_PATH="/home/ubuntu/"
-# Set the system folder path
 SYSTEM_PATH="system/"
-
-# Set the log file path and create a new entry with delimiter lines
 LOG_FILE="${BASE_PATH}${SYSTEM_PATH}suretide.log"
-printf '%.0s*' $(seq 1 $LINE_LENGTH) >> $LOG_FILE; echo >> $LOG_FILE
-printf '%.0s*' $(seq 1 $LINE_LENGTH) >> $LOG_FILE; echo >> $LOG_FILE
-
-# # Set the backup key file path
 BACKUP_KEY="${BASE_PATH}${SYSTEM_PATH}backup.pem"
-# Set the temporary directory path
 TEMPORARY_DIRECTORY="${BASE_PATH}${SYSTEM_PATH}temp"
 
-# Create the temporary directory if it does not exist
 mkdir -p "$TEMPORARY_DIRECTORY"
 
-# Check if the temporary directory exists
-if [ -d "$TEMPORARY_DIRECTORY" ]; then
-    echo "$TEMPORARY_DIRECTORY"
-else
-    echo "Failed to create temporary directory."
-fi
+echo "TEMPORARY_DIRECTORY: $TEMPORARY_DIRECTORY"
 
-# Set the backup server
-BACKUP_SERVER=""
-
-# Function to validate an IP address
 function valid_ip() {
   local ip=$1
   local stat=1
@@ -52,26 +25,14 @@ function valid_ip() {
   return $stat
 }
 
+BACKUP_SERVER=""
 if [ ! -z "$1" ]; then
   input="$1"
-  # Remove username if present
-  if [[ $input == *"@"* ]]; then
-    input="${input#*@}"
-  fi
-
-  # Remove https:// if present
-  if [[ $input == "https://"* ]]; then
-    input="${input#https://}"
-  fi
-
-    # Remove http:// if present
-  if [[ $input == "http://"* ]]; then
-    input="${input#http://}"
-  fi
-
+  input="${input#*@}"
+  input="${input#https://}"
+  input="${input#http://}"
+  
   if valid_ip "$input" || host "$input" > /dev/null 2>&1; then
-    # Update the BACKUP_SERVER variable in the current script
-    sed -i "s|BACKUP_SERVER=\"\"|BACKUP_SERVER=\"$1\"|" $0
     BACKUP_SERVER="$1"
   else
     echo "Error: Invalid IP address or hostname."
@@ -79,22 +40,11 @@ if [ ! -z "$1" ]; then
   fi
 fi
 
-# Calculate the length of the LOG_ENTRY_DATE_TIME text
 LOG_ENTRY_DATE_TIME="$(date +"%Y-%m-%d %H:%M:%S")"
-entry_length=${#LOG_ENTRY_DATE_TIME}
+echo "LOG ENTRY: $LOG_ENTRY_DATE_TIME" >> ${LOG_FILE}
 
-# Calculate the number of spaces needed to center the text
-spaces=$(( (line_length - entry_length) / 2 ))
-
-printf '%*.s' $spaces '' | tr ' ' '-' >> ${LOG_FILE}; echo -n " START OF LOG ENTRY " >> ${LOG_FILE}; printf '%*.s' $spaces '' | tr ' ' '-' >> ${LOG_FILE}; echo >>${LOG_FILE}
-printf '%*.s' $spaces '' | tr ' ' '-' >> ${LOG_FILE}; echo -n " $LOG_ENTRY_DATE_TIME " >> ${LOG_FILE}; printf '%*.s' $spaces '' | tr ' ' '-' >> ${LOG_FILE}; echo >> ${LOG_FILE}
-
-# Check if the backup.pem file exists
 if [ -e "$BACKUP_KEY" ]; then
-  # Get the file permissions
   file_permissions=$(stat -c "%a" "$BACKUP_KEY")
-
-  # Check if the permissions are not 400 or 600
   if [ "$file_permissions" != "400" ] && [ "$file_permissions" != "600" ]; then
     chmod 400 "$BACKUP_KEY"
     echo "File permissions for backup.pem have been changed to 400." >> "${LOG_FILE}"
@@ -103,13 +53,35 @@ else
   echo "backup.pem file not found." >> "${LOG_FILE}"
 fi
 
-
-# Stops specific services, where necessary
-echo "stopping rsync..." >> "${LOG_FILE}"
 sudo systemctl stop rsync >> "${LOG_FILE}"
-
-echo "starting rsync..." >> "${LOG_FILE}"
 sudo systemctl start rsync >> "${LOG_FILE}"
-
-echo "restarting rsync..." >> "${LOG_FILE}"
 sudo systemctl restart rsync >> "${LOG_FILE}"
+
+MYSQL_DATABASE="suretidewordpress"
+MYSQL_USER="suretidewordpressuser"
+MYSQL_PASSWORD="@Rp!!T431"
+
+if [ ! -z "$2" ]; then
+  sed -i "s|MYSQL_DATABASE=\"suretidewordpress\"|MYSQL_DATABASE=\"$2\"|" backupscript.sh
+fi
+
+if [ ! -z "$3" ]; then
+  sed -i "s|MYSQL_USER=\"suretidewordpressuser\"|MYSQL_USER=\"$3\"|" backupscript.sh
+fi
+
+if [ ! -z "$4" ]; then
+  sed -i "s|MYSQL_PASSWORD=\"@Rp!!T431\"|MYSQL_PASSWORD=\"$4\"|" backupscript.sh
+fi
+
+# Create the MySQL dump file path
+MYSQL_FILE="$TEMPORARY_DIRECTORY/database_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").sql"
+
+echo "🚦 🏁 Performing backup on $MYSQL_DATABASE database... 🏁 🚦" >> ${BASE_PATH}${$LOG_FILE}
+echo " ⚠️  Please ensure to update the IP address or domain URL of $MYSQL_DATABASE!! Or this script will not backup to your remote server. ⚠️" >> ${BASE_PATH}${$LOG_FILE}
+
+echo "Dumping MySQL database..." >> ${BASE_PATH}${$LOG_FILE}
+echo "Saved SQL file to $MYSQL_FILE." >> ${BASE_PATH}${$LOG_FILE}
+mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > "$MYSQL_FILE"
+
+echo "Using SFTP to transfer the file from this local server at http://$(curl ifconfig.me) to your remote server at http://$BACKUP_SERVER." >> ${BASE_PATH}${$LOG_FILE}
+echo -e "put $MYSQL_FILE\nexit" | $SFTP_LINE -o StrictHostKeyChecking=no -i $BACKUP_KEY $BACKUP_SERVER >> ${BASE_PATH}${$LOG_FILE}
