07aecf73a66a0b2aad6abe3a499343d8b7723e2a
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-07 20:52:23 +0100

    Update setup-part-1.sh

--- setup-part-1.sh
+++ setup-part-1.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
-# Check if arguments are provided
-if [ $# -eq 0 ]; then
-    # Ask for user input
-    read -p "Enter user@example.com or user@ip: " remote_server
-    read -p "Enter the key pair path to your server: " keypair_path
-    read -p "Enter the key pair path to your backup server: " backup_keypair_path
-    read -p "Enter the file path to transfer: " file_path
-    read -p "Enter the remote destination: " remote_destination
-else
-    # Use provided arguments
-    remote_server="$1"
-    keypair_path="$2"
-    backup_keypair_path="$3"
-    file_path="$4"
-    remote_destination="$5"
+# Prompt the user for input
+echo "Enter the remote server's username@ip (e.g. user@example.com):"
+read remote_server
+
+echo "Enter the path to the server's .pem or .ppk keypair:"
+read keypair_path
+
+echo "Enter the path of the file you want to upload:"
+read file_path
+
+echo "Enter the destination path on the remote server (e.g. /home/user/destination) or leave it empty for default (/home/username/):"
+read remote_destination
+
+# If the destination path is empty, use /home/username/
+if [ -z "$remote_destination" ]; then
+    username=$(echo "$remote_server" | cut -d '@' -f 1)
+    remote_destination="/home/${username}/"
 fi
 
-# Check if keypair files exist
-if [ ! -f "$keypair_path" ] || [ ! -f "$backup_keypair_path" ]; then
-    echo "Error: Key pair files not found."
+# Check if the keypair is .pem or .ppk
+key_ext="${keypair_path##*.}"
+
+# Test the connection
+if [ "$key_ext" == "pem" ]; then
+    ssh -i "${keypair_path}" "${remote_server}" exit
+    status=$?
+elif [ "$key_ext" == "ppk" ]; then
+    eval $(ssh-pageant -k "${keypair_path}")
+    ssh "${remote_server}" exit
+    status=$?
+else
+    echo "Invalid keypair extension. Please provide a .pem or .ppk keypair."
     exit 1
 fi
 
-# Get keypair file extensions
-key_ext=$(echo "${keypair_path##*.}")
-backup_key_ext=$(echo "${backup_keypair_path##*.}")
+if [ $status -ne 0 ]; then
+    echo "Connection failed. Please check your input and try again."
+    exit 1
+fi
 
-# Transfer files using rsync
+# Upload the file
 if [ "$key_ext" == "pem" ]; then
     # Use rsync with .pem keypair
-    rsync -avz -e "ssh -i \"$keypair_path\"" "$file_path" "$remote_server:$remote_destination"
+    rsync -avz -e "ssh -i \"${keypair_path}\"" "${file_path}" "${remote_server}:${remote_destination}"
 elif [ "$key_ext" == "ppk" ]; then
     # Use rsync with .ppk keypair (assuming ssh-pageant is installed and configured)
-    eval $(ssh-pageant -k "$keypair_path")
-    rsync -avz -e "ssh" "$file_path" "$remote_server:$remote_destination"
-else
-    echo "Error: Unsupported keypair file extension."
-    exit 1
+    eval $(ssh-pageant -k "${keypair_path}")
+    rsync -avz -e "ssh" "${file_path}" "${remote_server}:${remote_destination}"
 fi
 
-# Transfer files to backup server
-if [ "$backup_key_ext" == "pem" ]; then
-    # Use rsync with .pem keypair
-    rsync -avz -e "ssh -i \"$backup_keypair_path\"" "$file_path" "$remote_server:$remote_destination"
-elif [ "$backup_key_ext" == "ppk" ]; then
-    # Use rsync with .ppk keypair (assuming ssh-pageant is installed and configured)
-    eval $(ssh-pageant -k "$backup_keypair_path")
-    rsync -avz -e "ssh" "$file_path" "$remote_server:$remote_destination"
-else
-    echo "Error: Unsupported backup keypair file extension."
-    exit 1
-fi
+echo "File uploaded successfully."
 
-echo "File transfer completed."
+ssh -i "${keypair_path}" "${remote_server}"
