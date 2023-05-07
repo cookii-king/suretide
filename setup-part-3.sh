bab74c84a8481b539798bfbae80b438ad67c5af9
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-08 00:31:53 +0100

    Update setup-part-3.sh

--- setup-part-3.sh
+++ setup-part-3.sh
@@ -1,75 +1,39 @@
 #!/bin/bash
 
+function update_variables() {
+    local user="$1"
+    local server_address="$2"
+    local database="$3"
+
+    # Get the line numbers of the BACKUP_SERVER and MYSQL_DATABASE variables
+    local backup_server_line_number=$(grep -n "BACKUP_SERVER=" "$0" | cut -d : -f 1)
+    local mysql_database_line_number=$(grep -n "MYSQL_DATABASE=" "$0" | cut -d : -f 1)
+
+    # Update the BACKUP_SERVER and MYSQL_DATABASE variables in the script
+    sed -i "${backup_server_line_number}s|BACKUP_SERVER=\"\"|BACKUP_SERVER=\"${user}@${server_address}\"|" "$0"
+    sed -i "${mysql_database_line_number}s|MYSQL_DATABASE=\"\"|MYSQL_DATABASE=\"${database}\"|" "$0"
+}
+
+function create_crontab() {
+    local script_path="$(realpath "$0")"
+    (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/env bash -u ubuntu ${script_path} ${BACKUP_SERVER} ${MYSQL_DATABASE}") | crontab -
+}
+
 BASE_PATH="/home/ubuntu/"
 SYSTEM_PATH="system/"
 LOG_FILE="${BASE_PATH}${SYSTEM_PATH}suretide.log"
 BACKUP_KEY="${BASE_PATH}${SYSTEM_PATH}backup.pem"
-TEMPORARY_DIRECTORY="${BASE_PATH}${SYSTEM_PATH}temp"
-
-function valid_ip() {
-  local ip=$1
-  local stat=1
-
-  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    OIFS=$IFS
-    IFS='.'
-    ip=($ip)
-    IFS=$OIFS
-    [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-    stat=$?
-  fi
-  return $stat
-}
-
+TEMP_DIRECTORY="${BASE_PATH}${SYSTEM_PATH}temp/"
 BACKUP_SERVER=""
-if [ ! -z "$1" ]; then
-  input="$1"
-  input="${input#*@}"
-  input="${input#https://}"
-  input="${input#http://}"
-  
-  if valid_ip "$input" || host "$input" > /dev/null 2>&1; then
-    BACKUP_SERVER="$1"
-  else
-    echo "Error: Invalid IP address or hostname."
-    exit 1
-  fi
+MYSQL_DATABASE=""
+MYSQL_USER=""
+MYSQL_PASSWORD=""
+MYSQL_FILE="${TEMP_DIRECTORY}mysql_file.sql"
+WORDPRESS_DIRECTORY="/var/www/html/wordpress"
+NGINX_DIRECTORY="/etc/nginx"
+
+# Check if the script is called with the necessary arguments
+if [ "$#" -eq 3 ]; then
+    update_variables "$1" "$2" "$3"
+    create_crontab
 fi
-
-LOG_ENTRY_DATE_TIME="$(date +"%Y-%m-%d %H:%M:%S")"
-echo "LOG ENTRY: $LOG_ENTRY_DATE_TIME" >> ${LOG_FILE}
-
-if [ -e "$BACKUP_KEY" ]; then
-  file_permissions=$(stat -c "%a" "$BACKUP_KEY")
-  if [ "$file_permissions" != "400" ] && [ "$file_permissions" != "600" ]; then
-    chmod 400 "$BACKUP_KEY"
-    echo "File permissions for backup.pem have been changed to 400." >> "${LOG_FILE}"
-  fi
-else
-  echo "backup.pem file not found." >> "${LOG_FILE}"
-fi
-
-sudo systemctl stop rsync >> "${LOG_FILE}"
-sudo systemctl start rsync >> "${LOG_FILE}"
-sudo systemctl restart rsync >> "${LOG_FILE}"
-
-MYSQL_DATABASE="suretidewordpress"
-MYSQL_USER="suretidewordpressuser"
-MYSQL_PASSWORD="@Rp!!T431"
-
-if [ ! -z "$2" ]; then
-  sed -i "s|MYSQL_DATABASE=\"suretidewordpress\"|MYSQL_DATABASE=\"$2\"|" setup-part-3.sh
-fi
-
-if [ ! -z "$3" ]; then
-  sed -i "s|MYSQL_USER=\"suretidewordpressuser\"|MYSQL_USER=\"$3\"|" setup-part-3.sh
-fi
-
-if [ ! -z "$4" ]; then
-  sed -i "s|MYSQL_PASSWORD=\"@Rp!!T431\"|MYSQL_PASSWORD=\"$4\"|" setup-part-3.sh
-fi
-
-# Create the MySQL dump file path
-MYSQL_FILE="${TEMPORARY_DIRECTORY}database_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").sql"
-echo "${MYSQL_FILE}"
-echo -e "put $MYSQL_FILE\nexit" | $SFTP_LINE -o StrictHostKeyChecking=no -i $BACKUP_KEY $BACKUP_SERVER
