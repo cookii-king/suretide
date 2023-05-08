3343872df54de8ea7fd30fed5c65a37407a8aa2d
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-08 11:53:00 +0100

    Update setup-part-2.sh

--- setup-part-2.sh
+++ setup-part-2.sh
@@ -115,6 +115,10 @@ curl -sSL https://raw.githubusercontent.com/cookii-king/suretide/main/setup-part
 
 sudo mv setup-part-3.sh "${SYSTEM_PATH}backup.sh"
 
+curl -sSL https://raw.githubusercontent.com/cookii-king/suretide/main/diagnostics.sh -o diagnostics.sh && sudo chmod +x diagnostics.sh
+
+sudo mv diagnostics.sh "${SYSTEM_PATH}diagnostics.sh"
+
 sudo rm -r setup-part-2.sh
 echo "done ✅ ∙ to get rid of error just setup your wordpres and update the backup script to your liking..."
 echo "go to http://$(curl ifconfig.me) to see finish setting up your wordpress website. 😁"
