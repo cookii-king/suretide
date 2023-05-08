268d2387608ea64f25082442a449c11e235c6835
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-08 01:27:57 +0100

    Update setup-part-3.sh

--- setup-part-3.sh
+++ setup-part-3.sh
@@ -35,26 +35,56 @@ if [ "$#" -eq 5 ]; then
     sed -i "${mysql_user_line_number}s|MYSQL_USER=\"\"|MYSQL_USER=\"${mysql_user}\"|" "$0"
     sed -i "${mysql_password_line_number}s|MYSQL_PASSWORD=\"\"|MYSQL_PASSWORD=\"${mysql_password}\"|" "$0"
 
-    # Create crontab
+    # # Create crontab
+    # sudo crontab -u ubuntu -l > /tmp/c1
+
+    # CRON_JOB_LINE=" * * * * * $ENVIRONMENT_SHELL ${BASE_PATH}${SYSTEM_PATH}setup-part-3.sh ${BACKUP_SERVER} ${MYSQL_DATABASE} ${MYSQL_USER} ${MYSQL_PASSWORD} >> ${LOG_FILE} 2>&1"
+
+    # # Remove existing similar cron jobs
+    # grep -v "${BASE_PATH}${SYSTEM_PATH}setup-part-3.sh" /tmp/c1 > /tmp/c2
+    # mv /tmp/c2 /tmp/c1
+
+    # echo "Checking if crontab has automation for this script..." >> ${LOG_FILE}
+    # # Check if the cron job line is already in the crontab
+    # if ! grep -qF "$CRON_JOB_LINE" /tmp/c1; then
+    #     # If it's not in the crontab, append it to the temporary file
+    #     echo "Adding crontab automation to this script..." >> ${LOG_FILE}
+    #     echo "$CRON_JOB_LINE" >> /tmp/c1
+
+    #     # Install the modified crontab from the temporary file for the 'ubuntu' user
+    #     sudo crontab -u ubuntu /tmp/c1
+    # else
+    #     echo "The cron job is already in the crontab." >> ${LOG_FILE}
+    # fi
+
+    # # Clean up the temporary files
+    # rm /tmp/c1
+
+    #####################################################################################
+    # >>>>>>>>>>>>>>>>>>>>>>     Add Cron Job for Automation     <<<<<<<<<<<<<<<<<<<<<< #
+    #####################################################################################
+
+    # Save the current crontab for the 'ubuntu' user to a temporary file
     sudo crontab -u ubuntu -l > /tmp/c1
 
-    CRON_JOB_LINE=" * * * * * $ENVIRONMENT_SHELL ${BASE_PATH}${SYSTEM_PATH}setup-part-3.sh ${BACKUP_SERVER} ${MYSQL_DATABASE} ${MYSQL_USER} ${MYSQL_PASSWORD} >> ${LOG_FILE} 2>&1"
+    # Define the cron job line you want to add
+    CRON_JOB_LINE=" * * * * * $ENVIRONMENT_SHELL ${BASE_PATH}backupscript.sh >> ${BASE_PATH}backupscript.log 2>&1"
 
     # Remove existing similar cron jobs
-    grep -v "${BASE_PATH}${SYSTEM_PATH}setup-part-3.sh" /tmp/c1 > /tmp/c2
+    grep -v "${BASE_PATH}backupscript.sh" /tmp/c1 > /tmp/c2
     mv /tmp/c2 /tmp/c1
 
-    echo "Checking if crontab has automation for this script..." >> ${LOG_FILE}
+    echo "Checking if crontab has automation for this script..." >> ${BASE_PATH}backupscript.log
     # Check if the cron job line is already in the crontab
     if ! grep -qF "$CRON_JOB_LINE" /tmp/c1; then
-        # If it's not in the crontab, append it to the temporary file
-        echo "Adding crontab automation to this script..." >> ${LOG_FILE}
-        echo "$CRON_JOB_LINE" >> /tmp/c1
+    # If it's not in the crontab, append it to the temporary file
+    echo "Adding crontab automation to this script..." >> ${BASE_PATH}backupscript.log
+    echo "$CRON_JOB_LINE" >> /tmp/c1
 
-        # Install the modified crontab from the temporary file for the 'ubuntu' user
-        sudo crontab -u ubuntu /tmp/c1
+    # Install the modified crontab from the temporary file for the 'ubuntu' user
+    sudo crontab -u ubuntu /tmp/c1
     else
-        echo "The cron job is already in the crontab." >> ${LOG_FILE}
+    echo "The cron job is already in the crontab." >> ${BASE_PATH}backupscript.log
     fi
 
     # Clean up the temporary files
