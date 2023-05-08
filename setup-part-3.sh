4fc6dec3ce860e04bfcea4fa639cefc32c1db404
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-08 05:20:27 +0100

    Update setup-part-3.sh

--- setup-part-3.sh
+++ setup-part-3.sh
@@ -5,10 +5,10 @@ SYSTEM_PATH="system/"
 LOG_FILE="${BASE_PATH}${SYSTEM_PATH}suretide.log"
 BACKUP_KEY="${BASE_PATH}${SYSTEM_PATH}backup.pem"
 TEMP_DIRECTORY="${BASE_PATH}${SYSTEM_PATH}temp"
-BACKUP_SERVER="ubuntu@44.204.30.125"
-MYSQL_DATABASE="suretidewordpress"
-MYSQL_USER="root"
-MYSQL_PASSWORD="@Rp!!T431"
+BACKUP_SERVER="$1"
+MYSQL_DATABASE="$2"
+MYSQL_USER="$3"
+MYSQL_PASSWORD="$4"
 MYSQL_FILE="${TEMP_DIRECTORY}mysql_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").sql"
 WORDPRESS_DIRECTORY_TAR_FILE="${TEMP_DIRECTORY}wordpress_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").tar.gz"
 NGINX_DIRECTORY_TAR_FILE="${TEMP_DIRECTORY}nginx_backup_on_$(date +"%d_%m_%Y_at_%H_%M_%S").tar.gz"
@@ -16,6 +16,11 @@ WORDPRESS_DIRECTORY="/var/www/html/wordpress"
 NGINX_DIRECTORY="/etc/nginx"
 ENVIRONMENT_SHELL="/bin/bash"
 SFTP_LINE="/usr/bin/sftp"
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: sudo bash setup-part-3.sh <BACKUP_SERVER> <MYSQL_DATABASE> <MYSQL_USER> <MYSQL_PASSWORD>"
+    exit 1
+fi
 
 mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $MYSQL_FILE
 
@@ -30,7 +35,7 @@ tar -czf "$NGINX_DIRECTORY_TAR_FILE" "$NGINX_DIRECTORY"
 echo -e "put $NGINX_DIRECTORY_TAR_FILE\nexit" | $SFTP_LINE -o StrictHostKeyChecking=no -i $BACKUP_KEY $BACKUP_SERVER
 
 # Set up the cron job to run the backup script every minute
-CRON_JOB=" * * * * * $ENVIRONMENT_SHELL ${BASE_PATH}${SYSTEM_PATH}backup.sh >> $LOG_FILE 2>&1"
+CRON_JOB=" * * * * * $ENVIRONMENT_SHELL ${BASE_PATH}${SYSTEM_PATH}backup.sh $BACKUP_SERVER $MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD >> $LOG_FILE 2>&1"
 
 # Backup the current 'ubuntu' user's crontab
 sudo crontab -u ubuntu -l > /tmp/current_crontab
