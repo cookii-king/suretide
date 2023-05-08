a61cab1af68195dcb53fcee4d5a3457bcfc3bd51
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-05-08 01:01:24 +0100

    Update setup-part-3.sh

--- setup-part-3.sh
+++ setup-part-3.sh
@@ -4,20 +4,14 @@ function update_variables() {
     local user="$1"
     local server_address="$2"
     local database="$3"
-    local mysql_user="$4"
-    local mysql_password="$5"
 
-    # Get the line numbers of the BACKUP_SERVER, MYSQL_DATABASE, MYSQL_USER, and MYSQL_PASSWORD variables
+    # Get the line numbers of the BACKUP_SERVER and MYSQL_DATABASE variables
     local backup_server_line_number=$(grep -n "BACKUP_SERVER=" "$0" | cut -d : -f 1)
     local mysql_database_line_number=$(grep -n "MYSQL_DATABASE=" "$0" | cut -d : -f 1)
-    local mysql_user_line_number=$(grep -n "MYSQL_USER=" "$0" | cut -d : -f 1)
-    local mysql_password_line_number=$(grep -n "MYSQL_PASSWORD=" "$0" | cut -d : -f 1)
 
-    # Update the BACKUP_SERVER, MYSQL_DATABASE, MYSQL_USER, and MYSQL_PASSWORD variables in the script
+    # Update the BACKUP_SERVER and MYSQL_DATABASE variables in the script
     sed -i "${backup_server_line_number}s|BACKUP_SERVER=\"\"|BACKUP_SERVER=\"${user}@${server_address}\"|" "$0"
     sed -i "${mysql_database_line_number}s|MYSQL_DATABASE=\"\"|MYSQL_DATABASE=\"${database}\"|" "$0"
-    sed -i "${mysql_user_line_number}s|MYSQL_USER=\"\"|MYSQL_USER=\"${mysql_user}\"|" "$0"
-    sed -i "${mysql_password_line_number}s|MYSQL_PASSWORD=\"\"|MYSQL_PASSWORD=\"${mysql_password}\"|" "$0"
 }
 
 function create_crontab() {
@@ -48,6 +42,7 @@ function create_crontab() {
     rm /tmp/c1
 }
 
+
 BASE_PATH="/home/ubuntu/"
 SYSTEM_PATH="system/"
 LOG_FILE="${BASE_PATH}${SYSTEM_PATH}suretide.log"
@@ -62,8 +57,7 @@ WORDPRESS_DIRECTORY="/var/www/html/wordpress"
 NGINX_DIRECTORY="/etc/nginx"
 
 # Check if the script is called with the necessary arguments
-if [ "$#" -eq 5 ]; then
-    update_variables "$1" "$2" "$3" "$4" "$5"
+if [ "$#" -eq 3 ]; then
+    update_variables "$1" "$2" "$3"
     create_crontab
 fi
-
